5dbd6b3fe89592d19b3fb9a23f88c2c91303393d
Nicholas Juncos <nicholas.juncos@protonmail.com>
2022-03-18 17:27:20 -0500

    Fixed node install library

--- scripts/install_os_applications.sh
+++ scripts/install_os_applications.sh
@@ -12,7 +12,7 @@ sudo apt install python3-pip -y
 sudo apt install python3.9-distutils -y
 sudo apt install virtualenv -y
 
-sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install nodejs -y
 
 
