20aaff881f7c9211b5e150fc78061974732587d2
Nicholas Juncos <nicholas.juncos@protonmail.com>
2022-03-18 16:06:28 -0500

    Pathing for migrate

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /home/ubuntu/env/bin/activate
-cd /home/ubuntu/website/backend
-./manage.py migrate --no-input
+cd /home/ubuntu/website
+./backend/manage.py migrate --no-input
